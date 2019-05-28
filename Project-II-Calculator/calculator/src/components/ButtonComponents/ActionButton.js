6b1d396b26c03000b0b4c552d692aef019a51a6e
steven122185 <49796406+steven122185@users.noreply.github.com>
2019-05-28 17:49:43 -0400

    Finished the calculator and it is functional

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+export const ClearButton = (props) => (
+    <div className="clear-btn" onClick={props.handleClear}>
+        {props.children}
+    </div>
+)
+
+export default ClearButton;
